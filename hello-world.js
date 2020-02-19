eb80fd02beb8d070e1f476344e1ed550ae53ac44
Joaquin Rivas <jrivas@evopoinsolution.com>
2020-02-19 19:13:50 -0400

    book, addeds

--- hello-world.js
+++ hello-world.js
@@ -1,11 +1,9 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('hellow from the other side');
-})
+    res.send('Hello World, from express');
+});
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
-
-// https://stackabuse.com/building-a-rest-api-with-node-and-express/
